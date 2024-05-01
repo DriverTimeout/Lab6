faa3d335a0671786d7a38d6a13bd88816513b694
DriverTimeout <128652615+DriverTimeout@users.noreply.github.com>
2024-05-01 11:48:44 -0500

    Create FileCreator.h

---
+++ FileCreator.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <fstream>
+
+using namespace std;
+
+class FileCreator {
+private:
+	ofstream MyFile;
+public:
+	FileCreator(string);
+	void WriteFile(string, double, double, double, double, double);
+	void WriteHeader();
+	void CloseFile();
+};
